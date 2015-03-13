ac403d4e947cac7013a1cedece3b3a3788f805e4
Carter Lavering <carterlego2000@gmail.com>
2015-03-12 20:33:48 -0700

    Completely revised whole program

--- stock_get.py
+++ stock_get.py
@@ -1,108 +1,49 @@
-from lxml import html
-import requests
-import xlsxwriter as xlsx
 import time
+import requests
+from lxml import html
+import xlsxwriter
 
-# TO BE ADDED
-# [X] Time tracked during download (double alliteration FTW)
-# [ ] Extra columns (look at example spreadsheet)
+begin = time.time()
 
-# --------------------------------------------------------------------------- #
-signs = ['INTC', 'AAPL', 'MSFT', 'GOOG']
+## Modify this part as needed
+#  ================
 
-dates = ["1426809600", "1429228800", "1431648000", "1434672000", "1437091200",
-         "1440115200"]
-# --------------------------------------------------------------------------- #
+signs = ['INTC']
 
-headers = ['Sign', 'Date', 'Strike', 'Contract Name', 'Last', 'Bid', 'Ask',
-           'Change', '%Change', 'Volume', 'Open Interest',
-           'Implied Volatility']
+dates = [
+    '1426809600', '1429228800', '1431648000', '1434672000', '1437091200',
+    '1440115200']
+    
+excel = xlsxwriter.Workbook('stock.xlsx') # put filename here
 
-formatting = ['str', 'int', 'float', 'str', 'float', 'float', 'float', 'float',
-              'percent', 'int', 'int', 'percent']
+left_col = "//div[@id='optionsCallsTable']//tbody/tr"
 
+path = "//div[@id='optionsCallsTable']//tbody/tr[{}]/td/*//text()"
 
-def getStock(sign, date):
-    """Returns Yahoo Finance future info."""
+site = 'https://finance.yahoo.com/q/op?s={}&date={}' # Remember to call .format
+                                                     # if using somewhere else
+## Don't change this
+#  =================
 
-    # Download stock data into HTML tree
-    print("Downloading data for {} at {}...".format(sign, date))
-    page = requests.get(
-        "https://finance.yahoo.com/q/op?s={}+Options".format(sign))
-    tree = html.fromstring(page.text)
+ref = 0
 
-    # Create headers
-    headers = ['Strike', 'Contract Name', 'Last', 'Bid', 'Ask', 'Change',
-               '%Change', 'Volume', 'Open Interest', 'Implied Volatility']
+sheet = excel.add_worksheet()
 
-    # Get all text in each row
-    data = []
-    rows = tree.xpath('//div[@id="optionsCallsTable"]//tbody/tr')
-    for row in range(len(rows)):
-        toAppend = [sign, date]
+for sign in signs:
+    for date in dates:
+        print 'Getting', sign, str(date) + '...',
+        page = requests.get(site.format(sign, date))
+        tree = html.fromstring(page.text)
+        left_data = tree.xpath(left_col)
+        table = []
+        for row_n in range(len(left_data)):
+            table.append(tree.xpath(path.format(row_n + 1)))
+        for row_n, row in enumerate(table):
+            for col_n, cell in enumerate(row):
+                sheet.write(row_n + ref, col_n, cell)
+        ref += len(table) + 1   # Counting starts at 0
+        print 'Done'
+        
+end = time.time()
 
-        toAppend.extend(tree.xpath(
-            '//div[@id="optionsCallsTable"]//tbody/tr[{}]/td/*//text()'.format(
-                row + 1)))
-
-        data.append(toAppend)
-
-    # Finish and return
-    if data is not []:
-        return data
-
-
-def getStocks(signs, dates):
-    """Returns data for multiple stocks in list form."""
-
-    data = []
-    for sign in signs:
-        for date in dates:
-            data.extend(getStock(sign, date))
-
-    return data
-
-
-def printToExcel(data, filepath, x_offset=0, y_offset=0):
-    """Print a 2-dimensional list to an Excel file."""
-
-    workbook = xlsx.Workbook(filepath)
-    worksheet = workbook.add_worksheet()
-    percent = workbook.add_format({'num_format': '\%0.00'})
-    float = workbook.add_format({'num_format': '##0.00'})
-
-    print "Writing data..."
-    for row, row_data in enumerate(data):
-        for column, cell in enumerate(row_data):
-            if formatting[column] is 'percent':
-                worksheet.write(row + y_offset, column + x_offset, cell,
-                                percent)
-
-            elif formatting[column] is 'float':
-                worksheet.write(row + y_offset, column + x_offset, cell, float)
-
-            else:
-                worksheet.write(row + y_offset, column + x_offset, cell)
-
-    workbook.close()
-
-# --------------------------------------------------------------------------- #
-start = time.time()
-print 'Starting at {}'.format(start)
-data = getStocks(signs, dates)
-download_end = time.time()
-print 'Download completed in {} seconds'.format(round(download_end - start, 2))
-
-for row in data:
-    for i in range(len(row)):
-        if formatting[i] is 'percent':
-            row[i] = float(row[i][:-1])
-        else:
-            row[i] = eval('{}(row[i])'.format(formatting[i]))
-
-
-data.insert(0, headers)
-
-printToExcel(data, 'stockdata.xlsx')
-
-print 'Completed in {} seconds'.format(round(time.time() - start, 2))
+print 'Completed in', round(end - begin, 4), 'seconds'
