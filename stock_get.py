e380a773beed3f366f7f262039d0a946966ce606
Carter Lavering <carterlego2000@gmail.com>
2015-07-19 11:45:55 -0700

    Made dates to 2015 rather than 15
    Also added snazzy "Format" header

--- stock_get.py
+++ stock_get.py
@@ -270,7 +270,11 @@ except ZeroDivisionError:
         ' desktop and try again.'
     )
 
-# Format Data
+# \_\_\_\_\_    \_\_\_    \_\_\_\_    \_      \_  \_\_\_\_\_  \_\_\_\_\_
+#  \_          \_      \_  \_      \_  \_\_  \_\_  \_      \_      \_
+#   \_\_\_\_    \_      \_  \_\_\_\_    \_  \_  \_  \_\_\_\_\_      \_
+#    \_          \_      \_  \_    \_    \_      \_  \_      \_      \_
+#     \_            \_\_\_    \_      \_  \_      \_  \_      \_      \_
 
 headers = [
     'co_symbol', 'company', 'sector', 'industry', 'Last', 'Option', 'exp_date',
@@ -292,11 +296,12 @@ for sign in all_data:
     for date in [date for date in all_data[sign] if date != 'Info']:
         for r in all_data[sign][date][:]:
             # human-readable date = hrd
-            try:
-                hrd_lst = [r[2][-15:-9][x:x + 2] for x in range(0, 6, 2)]
-            except IndexError as ie:
-                raise IndexError(ie.args, r) from ie
-            hrd_str = '/'.join((hrd_lst[1], hrd_lst[2], hrd_lst[0]))
+            hrd_lst = [r[2][-15:-9][x:x + 2] for x in range(0, 6, 2)]
+            print(r[2], end=' ')
+            # Don't delete the extra parentheses, .join() only takes one
+            # argument         \/                                        \/
+            hrd_str = '/'.join((hrd_lst[1], hrd_lst[2], '20' + hrd_lst[0]))
+            print(hrd_str)
             row = ([sign] +
                    all_data[sign]['Info'][0:3] +
                    rearrange(r, [0, 2]) +
